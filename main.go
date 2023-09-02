4bf21e0ed3df91af83305befc61b2f46e4abc9c8
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-09-02 17:40:12 -0400

    hello world

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	app := fiber.New()
+
+	app.Get("/", func(context *fiber.Ctx) error {
+		return context.SendString("Hello World")
+	})
+
+	app.Listen(":3000")
+}
+
+func database() {
 	database, err := sql.Open("sqlite3", "./database.db")
 	if err != nil {
 		fmt.Println(err)
