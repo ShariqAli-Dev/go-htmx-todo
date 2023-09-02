dcf41916a37d2cbff6f3eddc192addacb0d5c94d
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-09-02 18:34:47 -0400

    setup fiber

--- main.go
+++ main.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Get("/", func(context *fiber.Ctx) error {
-		return context.SendString("Hello World")
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
 	})
+	// this is for things like assets or css or js files
+	app.Static("/", "./public")
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello World",
+		})
+	})
 	app.Listen(":3000")
 }
 
