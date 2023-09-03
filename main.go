aace670aac5fd35f6e5fc984d82da34b0f919764
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-09-03 14:11:32 -0400

    initial page is loadad with tasks from the db

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 type Task struct {
+	Id        int
 	Name      string
 	Completed bool
 }
@@ -24,7 +25,7 @@ func main() {
 	defer db.Close()
 
 	// intialize the database
-	initStatement, err := db.Prepare("create table if not exists task (id integer primany key, name text, completed boolean)")
+	initStatement, err := db.Prepare("create table if not exists task (id integer primary key AUTOINCREMENT, name text, completed boolean)")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -43,19 +44,41 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		// template an insert statement
-		insertStatement, err := db.Prepare("insert into task (name, completed) values (?, true)")
+		insertTaskStatement, err := db.Prepare("insert into task (name, completed) values (?, false)")
 		if err != nil {
+			fmt.Println(err)
 			return err
 		}
-		_, err = insertStatement.Exec("do the dishes")
+		_, err = insertTaskStatement.Exec("do the dishes")
 		if err != nil {
+			fmt.Println(err)
 			return err
 		}
 
-		tasks := []Task{
-			{Name: "wash dishes", Completed: false},
-			{Name: "take the dog out for a walk", Completed: true},
-			{Name: "water the plants", Completed: false},
+		// template for get tasks statement
+		var tasks []Task
+		var id int
+		var name string
+		var completed bool
+		rows, err := db.Query("select id, name, completed from task")
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err := rows.Scan(&id, &name, &completed)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			newTask := Task{
+				Id:        id,
+				Name:      name,
+				Completed: completed,
+			}
+			tasks = append(tasks, newTask)
 		}
 
 		return c.Render("index", fiber.Map{
@@ -64,55 +87,4 @@ func main() {
 	})
 
 	log.Fatal(app.Listen(":3000"))
-}
-
-func database() {
-	database, err := sql.Open("sqlite3", "./database.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer database.Close()
-
-	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	_, err = statement.Exec()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	statement2, err := database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	_, err = statement2.Exec("shariq", "ali")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var id int
-	var firstname string
-	var lastname string
-
-	rows, err := database.Query("SELECT id, firstname, lastname FROM people")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&id, &firstname, &lastname)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(id, firstname, lastname)
-	}
 }
